fed9a12c4b358950be6bcc19c2eb0ba8a870798b
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-09-13 09:33:50 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectScore() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultipyQuestion() {
+    
+}
